ef0321b88f8cd459517eebef6a6749f5e437ee08
yuufyu <2487857+yuufyu@users.noreply.github.com>
2024-01-07 20:06:36 +0900

    update

--- utils.py
+++ utils.py
@@ -5,13 +5,16 @@ def remove_all_invalid_items(
     self: bpy.types.AnyType,
     context: bpy.types.Context,
 ) -> None:
-    scn = context.scene
-    export_settings = scn.yfx_exporter_settings.export_settings
-    items = export_settings.collections
-    remove_indices = [
-        i for i, item in enumerate(items) if item is None or item.collection_ptr is None
-    ]
-    for i in remove_indices:
-        items.remove(i)
-    current_index = export_settings.collection_index
-    export_settings.collection_index = max(0, current_index - len(remove_indices))
+    if context and context.yfx_exporter_settings:
+        scn = context.scene
+        export_settings = scn.yfx_exporter_settings.export_settings
+        items = export_settings.collections
+        remove_indices = [
+            i
+            for i, item in enumerate(items)
+            if item is None or item.collection_ptr is None
+        ]
+        for i in remove_indices:
+            items.remove(i)
+        current_index = export_settings.collection_index
+        export_settings.collection_index = max(0, current_index - len(remove_indices))
