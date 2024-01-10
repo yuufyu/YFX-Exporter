5347f72253ffce072a992778daacbedab39ca470
yuufyu <2487857+yuufyu@users.noreply.github.com>
2024-01-11 00:12:42 +0900

    refactor

--- utils.py
+++ utils.py
@@ -1,30 +1,32 @@
 import bpy
 
-exclusive_remove_all_invalid_items = False
+exclusive_update_setting_items = False
 
 
-def remove_all_invalid_items(
+def remove_invalid_collection_settings(context: bpy.types.Context) -> None:
+    if context and context.scene.yfx_exporter_settings:
+        export_settings = context.scene.yfx_exporter_settings.export_settings
+        items = export_settings.collections
+        remove_indices = [
+            i
+            for i, item in enumerate(items)
+            if item is None or item.collection_ptr is None
+        ]
+        for i in remove_indices:
+            items.remove(i)
+        current_index = export_settings.collection_index
+        export_settings.collection_index = max(
+            0,
+            current_index - len(remove_indices),
+        )
+
+
+def update_setting_items(
     self: bpy.types.AnyType,
     context: bpy.types.Context,
 ) -> None:
-    global exclusive_remove_all_invalid_items  # noqa: PLW0603
-    if exclusive_remove_all_invalid_items is False:
-        exclusive_remove_all_invalid_items = True
-
-        if context and context.scene.yfx_exporter_settings:
-            export_settings = context.scene.yfx_exporter_settings.export_settings
-            items = export_settings.collections
-            remove_indices = [
-                i
-                for i, item in enumerate(items)
-                if item is None or item.collection_ptr is None
-            ]
-            for i in remove_indices:
-                items.remove(i)
-            current_index = export_settings.collection_index
-            export_settings.collection_index = max(
-                0,
-                current_index - len(remove_indices),
-            )
-
-        exclusive_remove_all_invalid_items = False
+    global exclusive_update_setting_items  # noqa: PLW0603
+    if exclusive_update_setting_items is False:
+        exclusive_update_setting_items = True
+        remove_invalid_collection_settings(context)
+        exclusive_update_setting_items = False
